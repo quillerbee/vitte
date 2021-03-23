36a7e5e3858cf64cba544dfc2092bba175edcd7a
abhijit-kar <reachme@abhijit-kar.com>
2021-03-23 12:56:02 +0530

    Hover state added for links

--- src/pages/_folder.svelte
+++ src/pages/_folder.svelte
@@ -48,12 +48,12 @@
 	<slot />
 
 	<p>
-		Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+		Visit <a class="hover:underline" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
 		apps.
 	</p>
 
 	<p>
-		Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a>
+		Check out <a class="hover:underline" href="https://github.com/sveltejs/kit#readme">SvelteKit</a>
 		for the officially supported framework, also powered by Vite!
 	</p>
 </main>

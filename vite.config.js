44ebb4750676bccd071e6f904e8f5b318b87e97f
abhijit-kar <reachme@abhijit-kar.com>
2021-03-23 10:25:40 +0530

    ES Lint & Prettier Introduced

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite'
-import svelte from '@sveltejs/vite-plugin-svelte'
-import { getAliases } from 'vite-aliases'
+import { defineConfig } from "vite";
+import svelte from "@sveltejs/vite-plugin-svelte";
+import { getAliases } from "vite-aliases";
 
 const aliases = getAliases();
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
-  return {
-    resolve: {
-      alias: aliases
-    },
-    plugins: [svelte()],
-    build: {
-      minify: isProduction,
-      brotliSize: false, // To Speed Up Build
-      rollupOptions: {
-        // Disabled Hashing as Netlify Does Hashing for us using Etag.
-        output: {
-          entryFileNames: `assets/[name].js`,
-          chunkFileNames: `assets/[name].js`,
-        }
-      }
-    }
-  };
+	const isProduction = mode === "production";
+	return {
+		resolve: {
+			alias: aliases,
+		},
+		plugins: [svelte()],
+		build: {
+			minify: isProduction,
+			brotliSize: false, // To Speed Up Build
+			rollupOptions: {
+				// Disabled Hashing as Netlify Does Hashing for us using Etag.
+				output: {
+					entryFileNames: `assets/[name].js`,
+					chunkFileNames: `assets/[name].js`,
+				},
+			},
+		},
+	};
 });
